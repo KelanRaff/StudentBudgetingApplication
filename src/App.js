806351cb1bc26824e971b6e60ee9566d77e90870
unknown <kelanrafferty@gmail.com>
2022-12-05 15:59:55 +0000

    Chart.js functionality added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import Graph from './Components/Graph';
 
 function App() {
   return (
     <div className="App">
-      <h1>Student Budgeting App</h1>
+      <div className="Container mx-auto max-w-6xl text-center drop-shadow-lg text-gray-800">
+        <h1 className="text-4xl py-8 mb-10 bg-slate-800 text-white rounded">Student Budgeting App</h1>
+
+          {/* Grid Columns*/}
+          <div className="grid md: grid-cols-2 gap-4">
+            <Graph></Graph>
+          </div>
+      </div>
     </div>
   );
 }
