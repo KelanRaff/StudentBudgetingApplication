a19fc89739913114b4e04544ab0a905de395b639
KELAN RAFFERTY <G00382023@gmit.ie>
2023-02-25 00:17:11 +0000

    mongoDB connection established

--- Server/server.js
+++ Server/server.js
@@ -9,10 +9,24 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+//mongoDb Connection
+const con = require('./db/connection.js');
+
 //using routes
 app.use(require('./routes/route'));
 
+//List to http server
+con.then(db =>{
+    if(!db) return process.exit(1);
 
-app.listen(port, () =>{
-    console.log('server is running on port: ' + port);
+    app.listen(port, () =>{
+        console.log('server is running on port: ' + port);
+    })
+
+    app.on('error', err => console.log('Failed to Connect with HTTP Server: ' + err))
+    //error in mongoDb connection
+}).catch(error => {
+    console.log('Connection Failed....!')
 })
+
+
