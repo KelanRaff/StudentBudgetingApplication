f540606e5af2bcfa7f936bde339d5d90aff25aea
KELAN RAFFERTY <G00382023@gmit.ie>
2023-02-27 20:50:42 +0000

    Backend HTTP Services added

--- Server/controller/controller.js
+++ Server/controller/controller.js
@@ -1,18 +1,66 @@
 const model = require('../models/model');
+const { merge } = require('../routes/route');
 
-//post
-function create_Categories(req,res){
+//post http://localhost:8080/api/catagories
+async function create_Categories(req,res){
     const Create = new model.Categories({
         type:"Investment",
         color:'#FCBE44',
     })
 
-    Create.save(function(err){
+    await Create.save(function(err){
         if(!err) return res.json(Create);
         return res.status(400).json({message : 'Error while creating categories:' + err});
     })
 }
 
+//get http://localhost:8080/api/catagories
+async function get_Categories(req,res){
+    let data = await model.Categories.find({})
+
+    let filter = await data.map(v => Object.assign({},{type: v.type, color: v.color}));
+    return res.json(filter);
+}
+
+//post http://localhost:8080/api/transaction
+async function create_Transaction(req,res){
+    if(!req.body) return res.status(400).json("Post Http Data Not Provided");
+    let {name, type, amount} = req.body;
+
+    const create = await new model.Transaction(
+        {
+            name,
+            type,
+            amount,
+            date: new Date()
+        }
+    );
+
+    create.save(function(err){
+        if(!err) return res.json(create);
+        return res.status(400).json({message: 'Error while creating transaction' + err});
+    });
+}
+
+//get http://localhost:8080/api/transaction
+async function get_Transaction(req,res){
+    let data = await model.Transaction.find({});
+    return res.json(data);
+}
+
+//get http://localhost:8080/api/transaction
+async function delete_transaction(req,res){
+    if(!req.body) res.status(400).json({message: "Request body not found"});
+    await model.Transaction.deleteOne(req.body, function(err){
+        if(!err) res.json("Record Deleted...!");
+    }).clone().catch(function(err){res.json("Error while deleting Transaction Record")})
+}
+
+
 module.exports={
-    create_Categories
+    create_Categories,
+    get_Categories,
+    create_Transaction,
+    get_Transaction,
+    delete_transaction
 }
--- Server/routes/route.js
+++ Server/routes/route.js
@@ -2,6 +2,12 @@ const routes = require('express').Router();
 const controller = require('../controller/controller');
 
 routes.route('/api/catagories')
-    .post(controller.create_Categories);
+    .post(controller.create_Categories)
+    .get(controller.get_Categories)
+
+routes.route('/api/transaction')
+    .post(controller.create_Transaction)
+    .get(controller.get_Transaction)
+    .delete(controller.delete_transaction)
 
 module.exports=routes;
